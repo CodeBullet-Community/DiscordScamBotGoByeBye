3ea6b9b5f581011a72bfa9481788da521deffe10
Pagwin <git@pagwin.xyz>
2022-02-07 12:53:15 -0500

    added logging and have the basic structure setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+extern crate serenity;
+extern crate tokio;
+extern crate serde;
+extern crate log;
+extern crate env_logger;
+
+use serenity::{Client, framework, client::{EventHandler, Context}, model::channel::Message};
+use log::error;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    let client = Client::builder(std::env::var("DISCORD_TOKEN").expect("no env var DISCORD_TOKEN"))
+        .framework(framework::StandardFramework::new());
+}
+
+struct Handler {}
+
+#[serenity::async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, context: Context, message:Message){
+        let prob = calc_spam_probability(&message).await;
+        if prob >= 1.0 {
+            match message.delete(context).await {
+                Ok(_)=>{},
+                Err(err)=> error!("Error on message delete(due to spam): {}",err),
+            }
+        }
+    }
+}
+//I don't like how I'm returning an f64 instead of a value restricted to 0 <= ret <= 1 so I may do
+//something about that in the future
+async fn calc_spam_probability(message:&Message)->f64 {
+    0.0
 }
