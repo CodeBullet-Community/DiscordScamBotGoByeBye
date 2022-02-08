5183de103720efc1ef08ba599af887e13994f280
Pagwin <git@pagwin.xyz>
2022-02-08 16:45:57 -0500

    now all the config file types are handled

--- src/utils.rs
+++ src/utils.rs
@@ -5,30 +5,98 @@ extern crate serde_json;
 #[cfg(feature = "ini")]
 extern crate serde_ini;
 #[cfg(feature = "toml")]
-extern crate serde_yaml;
+extern crate toml;
 #[cfg(feature = "yaml")]
 extern crate serde_yaml;
 #[cfg(feature = "env")]
 extern crate envy;
+use std::{fs::read_to_string, fmt::Debug};
+
 use serde::{Serialize,Deserialize};
 use dirs::config_dir;
 
 
 
-#[derive(Serialize,Deserialize)]
-pub struct Config{
+#[derive(Serialize,Deserialize,Clone)]
+struct InternConfig{
+    token:Option<String>
+}
+pub struct Config {
     token:String
+}
+#[derive(Debug)]
+enum ConfigAttrMissing{TOKEN}
+impl InternConfig{
+    fn verify(self)->Result<Config,ConfigAttrMissing> {
+        let token = match self.token {
+            Some(token)=>token,
+            None=> return Err(ConfigAttrMissing::TOKEN)
+        };
+        Ok(Config{token})
+    }
+}
+trait Combinable {
+    fn union(self, other:Self)->Self;
+}
+impl Combinable for InternConfig {
+    //there may be a double backflippy macro way of handling this but for now the effort isn't
+    //worth it
+    fn union(mut self, other: Self)->Self{
+        if other.token.is_some(){
+            self.token = Some(self.token.unwrap_or(other.token.unwrap()));
+        }
+        self
+    }
+}
+impl<T:Combinable> Combinable for Option<T>{
+    fn union(mut self, other:Self)->Self{
+        if other.is_none(){
+            self
+        }
+        else if self.is_none() {
+            other
+        }
+        else {
+            Some(self.unwrap().union(other.unwrap()))
+        }
+    }
 }
 //level of priority for each config type if multiple are enabled and available
 // env > yaml > toml > ini > json
-pub fn get_config()->Option<Config>{
-    let mut config:Option<Config> = None;
+pub fn get_config()->Config{
+    let mut config:Option<InternConfig> = None;
+    #[cfg(feature = "env")]
+    {
+        config = match envy::from_env::<InternConfig>() {
+            Ok(cfg)=>Some(cfg),
+            Err(_)=>None
+        }
+    }
+    #[cfg(feature = "yaml")]
+    {
+        if let Ok(file_dat) = read_to_string(config_file_loc("yaml")){
+            config = config.union(serde_yaml::from_str(file_dat.as_str()).unwrap());
+        }
+    }
+    #[cfg(feature = "toml")]
+    {
+        if let Ok(file_dat) = read_to_string(config_file_loc("toml")){
+            config = config.union(toml::from_str(file_dat.as_str()).unwrap());
+        }
+    }
+    #[cfg(feature = "ini")]
+    {
+        if let Ok(file_dat) = read_to_string(config_file_loc("ini")){
+            config = config.union(serde_ini::from_str(file_dat.as_str()).unwrap());
+        }
+    }
     #[cfg(feature = "json")]
     {
-        let tmp:Config = serde_json::from_str();
-
+        if let Ok(file_dat) = read_to_string(config_file_loc("json")){
+            config = config.union(serde_json::from_str(file_dat.as_str()).unwrap());
+        }
     }
-    config
+    config.expect("no config provided").verify().unwrap()
 }
 fn config_file_loc(filetype:&str) -> String {
     config_dir().unwrap()
