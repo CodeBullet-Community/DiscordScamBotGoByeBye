8d7c7a004de990b91124b42aa92d97670ea8cc13
Pagwin <git@pagwin.xyz>
2022-02-08 15:41:33 -0500

    removed unnecessary stuff that I wasted time on

--- src/utils.rs
+++ src/utils.rs
@@ -11,28 +11,10 @@ extern crate serde_yaml;
 #[cfg(feature = "env")]
 extern crate envy;
 use serde::{Serialize,Deserialize};
-use lazy_static::lazy_static;
 use dirs::config_dir;
 
 
-//sets a bunch of consts up namely
-// CONFIG_FILE_TYPES
-include!(concat!(env!("OUT_DIR"), "/feature_gen.rs"));
 
-lazy_static! {
-    static ref CONFIG_LOCS:Vec<String> = 
-        CONFIG_FILE_TYPES.iter().map(
-            |cft|{
-                config_dir().unwrap()
-                    .join(env!("CARGO_PKG_NAME"))
-                    .join({
-                        let mut t = "config.".to_string();
-                        t.push_str(cft);
-                        t
-                    })
-                    .to_str().unwrap().to_string()
-            }).collect();
-}
 #[derive(Serialize,Deserialize)]
 pub struct Config{
     token:String
@@ -43,8 +25,14 @@ pub fn get_config()->Option<Config>{
     let mut config:Option<Config> = None;
     #[cfg(feature = "json")]
     {
-        let tmp = 
+        let tmp:Config = serde_json::from_str();
+
     }
-    
     config
+}
+fn config_file_loc(filetype:&str) -> String {
+    config_dir().unwrap()
+        .join(env!("CARGO_PKG_NAME"))
+        .join(format!("config.{}",filetype)).to_str().unwrap().to_string()
+
 }
