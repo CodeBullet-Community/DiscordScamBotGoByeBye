1848dc90cca931e85fffdea2f8f1ab1309a96ecf
Pagwin <git@pagwin.xyz>
2022-02-11 09:48:58 -0500

    a bunch of logging stuff

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ extern crate lazy_static;
 mod utils;
 use regex::Regex;
 use serenity::{Client, framework, client::{EventHandler, Context}, model::{channel::Message, prelude::Ready}, Error};
-use log::{info,error};
+use log::*;
 use lazy_static::lazy_static;
 
 lazy_static!{
@@ -18,7 +18,9 @@ lazy_static!{
 #[tokio::main]
 async fn main() {
     env_logger::init();
+    debug!("Logger initialized");
     let config = utils::get_config();
+    debug!("Config gotten");
     let client = Client::builder(config.token)
         .framework(framework::StandardFramework::new())
         .event_handler(Handler);
@@ -30,7 +32,7 @@ struct Handler;
 #[serenity::async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, _context:Context, _ready:Ready){
-        info!("bot is ready");
+        info!("Bot is ready");
     }
     async fn message(&self, context: Context, message:Message){
         info!("Message: {}\t[#{}]({}): {}",
@@ -38,18 +40,22 @@ impl EventHandler for Handler {
             message.channel(&context).await.unwrap().guild().unwrap().name,
             message.author.name,
             message.content);
-
+        
         let prob:f64 = match calc_spam_probability(&message,&context).await{
             Ok(v)=>v,
             Err(e)=>{
-                error!("got error calculating spam probability: {}", e);
+                error!("got error calculating spam probability: {} with message {}[{}]({}):{}", e,
+                    message.guild(&context).await.expect("The guild or message vanished as we queried it").name,
+                    message.channel(&context).await.unwrap().guild().unwrap().name,
+                    message.author.name,
+                    message.content);
                 return
             }
         };
         if prob > 0.5 {
             match message.delete(context).await {
                 Ok(_)=>{},
-                Err(err)=> error!("Error on message delete(due to spam): {}",err),
+                Err(err)=> error!("Error on message delete: {}",err),
             }
         }
     }
@@ -58,10 +64,11 @@ impl EventHandler for Handler {
 //something about that in the future
 //note I'm intending on having the handled at least in part by a naive bayes classifier which is
 //why a probability is returned
-// context requirement should only be temporary for this
+// probably want to create a custom error type so ? can be used for all results not just serenity
+// results
 /// Currently a reimplementation of the naive regex algo that was in the original aaaaaaaaaaaa
 async fn calc_spam_probability(message:&Message,context:&Context)->Result<f64,Error> {
-
+    //naive method of determining modmin status but it's fine for now
     let author_mod = match message.guild(context).await.ok_or("Missing Guild").unwrap().role_by_name("Moderator"){
         Some(role)=>message.author.has_role(context,
             message.guild(context).await.ok_or("Missing Guild").unwrap().id,
@@ -70,12 +77,22 @@ async fn calc_spam_probability(message:&Message,context:&Context)->Result<f64,Er
     };
 
     if message.author.bot {
+        trace!("{} is a bot",message.author);
         Ok(0.0)
     }
-    else if message.content.contains("@everyone") 
-        && LINK_REGEX.is_match(message.content.as_str())
-        && author_mod{
-        Ok(1.0)
+    else if author_mod {
+        trace!("{} is a mod on {}",message.author,
+            message.guild(context).await.ok_or("Missing Guild").unwrap().name);
+        Ok(0.0)
     }
-    else {Ok(0.0)}
+    else { 
+        trace!("Basic user checks complete {} is not a mod or bot on {}",message.author.name, 
+            message.guild(context).await.ok_or("Missing Guild").unwrap().name);
+        if message.content.contains("@everyone") 
+            && LINK_REGEX.is_match(message.content.as_str()){
+            debug!("\"{}\" from {} has been determined to be a spam message",message.content,message.author.name);
+            Ok(1.0)
+        }
+        else {Ok(0.0)}
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,6 @@
 extern crate serde;
 extern crate dirs;
+extern crate log;
 #[cfg(feature = "json")]
 extern crate serde_json;
 #[cfg(feature = "ini")]
@@ -10,11 +11,12 @@ extern crate toml;
 extern crate serde_yaml;
 #[cfg(feature = "env")]
 extern crate envy;
-use std::{fmt::Debug};
 
+use std::fmt::Debug;
 use serde::{Serialize,Deserialize};
 use dirs::config_dir;
-use std::fs::read_to_string;
+use log::*;
+
 
 #[derive(Serialize,Deserialize,Clone)]
 struct InternConfig{
@@ -34,6 +36,17 @@ impl InternConfig{
         Ok(Config{token})
     }
 }
+impl Debug for InternConfig {
+    fn fmt(&self, formatter:&mut std::fmt::Formatter)->Result<(),std::fmt::Error>{
+        if self.token.is_some() {
+            formatter.write_str("{")?;
+            formatter.write_str(" token: ")?;
+            formatter.write_str(self.token.clone().unwrap().as_str())?;
+            formatter.write_str(" }")?;
+        }
+        Ok(())
+    }
+}
 trait Combinable {
     fn union(self, other:Self)->Self;
 }
@@ -41,6 +54,7 @@ impl Combinable for InternConfig {
     //there may be a double backflippy macro way of handling this but for now the effort isn't
     //worth it
     fn union(mut self, other: Self)->Self{
+
         if other.token.is_some(){
             self.token = Some(self.token.unwrap_or(other.token.unwrap()));
         }
@@ -69,38 +83,45 @@ pub fn get_config()->Config{
         config = match envy::from_env::<InternConfig>() {
             Ok(cfg)=>Some(cfg),
             Err(_)=>None
-        }
+        };
+        trace!("config after env vars {:?}",config);
     }
     #[cfg(feature = "yaml")]
     {
         if let Ok(file_dat) = read_to_string(config_file_loc("yaml")){
             config = config.union(serde_yaml::from_str(file_dat.as_str()).unwrap());
         }
+        trace!("config after yaml vars {:?}",config);
     }
     #[cfg(feature = "toml")]
     {
         if let Ok(file_dat) = read_to_string(config_file_loc("toml")){
             config = config.union(toml::from_str(file_dat.as_str()).unwrap());
         }
+        trace!("config after toml vars {:?}",config);
     }
     #[cfg(feature = "ini")]
     {
         if let Ok(file_dat) = read_to_string(config_file_loc("ini")){
             config = config.union(serde_ini::from_str(file_dat.as_str()).unwrap());
         }
+        trace!("config after ini vars {:?}",config);
     }
     #[cfg(feature = "json")]
     {
         if let Ok(file_dat) = read_to_string(config_file_loc("json")){
             config = config.union(serde_json::from_str(file_dat.as_str()).unwrap());
         }
+        trace!("config after json vars {:?}",config);
     }
+    debug!("complete pre-verified config {:?}",config);
     //this should be rewritten so users don't get bad error messages when they don't have a config
     config.expect("no config provided").verify().expect("config was missing a value or otherwise couldn't be verified")
 }
 pub fn config_file_loc(filetype:&str) -> String {
-    config_dir().unwrap()
+    let loc = config_dir().unwrap()
         .join(env!("CARGO_PKG_NAME"))
-        .join(format!("config.{}",filetype)).to_str().unwrap().to_string()
-
+        .join(format!("config.{}",filetype)).to_str().unwrap().to_string();
+    debug!("{} filetype is located at {}",filetype,loc);
+    loc
 }
