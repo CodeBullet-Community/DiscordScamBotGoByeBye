b35b3766984565b92cd240f0d097d5fb39e88866
Pagwin <git@pagwin.xyz>
2022-02-11 11:09:21 -0500

    moved the calc spam probability function into it's own module for ease of testing

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,12 @@ extern crate tokio;
 extern crate serde;
 extern crate log;
 extern crate env_logger;
-extern crate regex;
-extern crate lazy_static;
 mod utils;
-use regex::Regex;
-use serenity::{Client, framework, client::{EventHandler, Context}, model::{channel::Message, prelude::Ready}, Error};
-use log::*;
-use lazy_static::lazy_static;
+mod prob;
 
-lazy_static!{
-    static ref LINK_REGEX:Regex = Regex::new("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|)+\\.([a-zA-Z]+)").expect("Regex failed to compile unexpectedly");
-}
+use serenity::{Client, framework, client::{EventHandler, Context}, model::{channel::Message, prelude::Ready}};
+use log::*;
+use prob::calc_spam_probability;
 
 #[tokio::main]
 async fn main() {
@@ -58,41 +53,5 @@ impl EventHandler for Handler {
                 Err(err)=> error!("Error on message delete: {}",err),
             }
         }
-    }
-}
-//I don't like how I'm returning an f64 instead of a value restricted to 0 <= ret <= 1 so I may do
-//something about that in the future
-//note I'm intending on having the handled at least in part by a naive bayes classifier which is
-//why a probability is returned
-// probably want to create a custom error type so ? can be used for all results not just serenity
-// results
-/// Currently a reimplementation of the naive regex algo that was in the original aaaaaaaaaaaa
-async fn calc_spam_probability(message:&Message,context:&Context)->Result<f64,Error> {
-    //naive method of determining modmin status but it's fine for now
-    let author_mod = match message.guild(context).await.ok_or("Missing Guild").unwrap().role_by_name("Moderator"){
-        Some(role)=>message.author.has_role(context,
-            message.guild(context).await.ok_or("Missing Guild").unwrap().id,
-            role).await?,
-        None => false
-    };
-
-    if message.author.bot {
-        trace!("{} is a bot",message.author);
-        Ok(0.0)
-    }
-    else if author_mod {
-        trace!("{} is a mod on {}",message.author,
-            message.guild(context).await.ok_or("Missing Guild").unwrap().name);
-        Ok(0.0)
-    }
-    else { 
-        trace!("Basic user checks complete {} is not a mod or bot on {}",message.author.name, 
-            message.guild(context).await.ok_or("Missing Guild").unwrap().name);
-        if message.content.contains("@everyone") 
-            && LINK_REGEX.is_match(message.content.as_str()){
-            debug!("\"{}\" from {} has been determined to be a spam message",message.content,message.author.name);
-            Ok(1.0)
-        }
-        else {Ok(0.0)}
     }
 }
