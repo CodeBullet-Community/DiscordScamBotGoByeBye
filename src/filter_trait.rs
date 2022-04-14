99ff286bd212324ac2e2355968345079ac8392a9
Pagwin <git@pagwin.xyz>
2022-04-14 18:56:48 -0400

    internal: added Xor filter and made And filter less shit

--- src/filter_trait.rs
+++ src/filter_trait.rs
@@ -1,12 +1,13 @@
 extern crate serenity;
+
 use serenity::{client::Context, model::channel::Message};
 
-//split into 2 traits 1 &self one &mut self after dealing with dumb trait objects
+//TODO:split into 2 traits 1 &self one &mut self after dealing with dumb trait objects
 pub trait FilterTrait where Self:Send+Sync{
     fn should_act(&self, message:&Message ,context:&Context)->bool;
     //don't know why 'static is needed so that could be a problem in the future
-    fn and<T: 'static>(self, other:T)->AndFilter where T:FilterTrait, Self:'static + Sized{
-        return AndFilter(Box::new(self),Box::new(other))
+    fn and<T>(self, other:T)->AndFilter<Self,T> where T:FilterTrait, Self:Sized{
+        return AndFilter(self,other)
     }
     fn negate(self)->NegateFilter<Self> where Self:Sized{
         return NegateFilter(self)
@@ -14,15 +15,21 @@ pub trait FilterTrait where Self:Send+Sync{
     fn or<T>(self, other:T)->OrFilter<Self,T> where T:FilterTrait, Self:Sized{
         return OrFilter(self, other)
     }
-
+    fn xor<T>(self, other:T)->XorFilter<Self,T> where T:FilterTrait, Self:Sized{
+        return XorFilter(self, other)
+    }
 }
-//in hindsight these should be generics not trait objects
-pub struct AndFilter(Box<dyn FilterTrait>, Box<dyn FilterTrait>);
-impl FilterTrait for AndFilter {
+
+pub struct AndFilter<T,U>(T, U) where T:FilterTrait, U:FilterTrait;
+impl<T,U> FilterTrait for AndFilter<T,U> where T:FilterTrait, U:FilterTrait {
     fn should_act(&self, message:&Message, context:&Context) ->bool {
         return self.0.should_act(message, context) && self.1.should_act(message, context)
     }
 }
+
+//unneeded and enables less readable code but I can't help myself
+//impl
+
 pub struct NegateFilter<T>(T) where T:FilterTrait;
 impl<T> FilterTrait for NegateFilter<T> where T:FilterTrait{
     fn should_act(&self, message:&Message, context:&Context) ->bool {
@@ -36,9 +43,18 @@ impl<T,U> FilterTrait for OrFilter<T,U> where T:FilterTrait, U:FilterTrait{
         self.0.should_act(message, context) || self.1.should_act(message, context)
     }
 }
-//this may be dumb
+
+pub struct XorFilter<T,U>(T,U) where T:FilterTrait, U:FilterTrait;
+impl<T,U> FilterTrait for XorFilter<T,U> where T:FilterTrait, U:FilterTrait{
+    fn should_act(&self, message:&Message, context:&Context) ->bool {
+        self.0.should_act(message, context) ^ self.1.should_act(message, context)
+    }
+}
+
+//this may be dumb but I don't think it is
 impl<T> FilterTrait for T where T:Fn(&Message,&Context)->bool + Send + Sync{
     fn should_act(&self, message:&Message, context:&Context) ->bool {
         self(message,context)
     }
 }
+
