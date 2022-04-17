22c45a2a665c6c20858d6770523dadd52106d877
Pagwin <git@pagwin.xyz>
2022-04-17 16:38:52 -0400

    fix: doesn't false positive if the role is on the server anymore but false negatives now

--- src/filters/role_filter.rs
+++ src/filters/role_filter.rs
@@ -9,12 +9,19 @@ pub struct RoleFilter{role_name:String}
 
 impl FilterTrait for RoleFilter{
     fn should_act(&self, message:&serenity::model::channel::Message, context:&serenity::client::Context) -> bool {
-        if let Some(v) = executor::block_on(message.guild(context)){
-            let ret = v.clone().role_by_name(self.role_name.as_str()).clone().as_ref().is_some();
+        if let Some(guild) = executor::block_on(message.guild(context)){
+            let ret = if let Some(role) = guild.clone().role_by_name(self.role_name.as_str()).clone().as_ref(){
+                let has_role_monad = message.member.clone().map(|mem|mem.user.map(|u| executor::block_on(u.has_role(context, guild.id, role.id)))).flatten();
+                //annoyed I can't use flatten for this
+                has_role_monad.ok_or(serenity::prelude::SerenityError::Other("hush")).unwrap_or(Ok(false)).unwrap_or(false)
+            }
+            else{
+                false
+            };
             trace!("is role {} had? {}",self.role_name,ret);
             return ret
         }
-        false
+        return false
     }
 }
 impl From<&str> for RoleFilter {
