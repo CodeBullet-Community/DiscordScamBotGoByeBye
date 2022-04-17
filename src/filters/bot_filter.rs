ca6a582b1f2c1c47494d2d3532811ad9d9951205
Pagwin <git@pagwin.xyz>
2022-04-17 16:04:22 -0400

    fix: made bot filter not use futures::executor because it didn't work however there is a possibility for false negatives now

--- src/filters/bot_filter.rs
+++ src/filters/bot_filter.rs
@@ -9,7 +9,7 @@ pub struct BotFilter;
 
 impl FilterTrait for BotFilter{
     fn should_act(&self, message:&serenity::model::channel::Message, context:&serenity::client::Context) -> bool {
-        let ret = executor::block_on(message.member(context)).map(|mem|mem.user.bot).unwrap_or(false);
+        let ret = message.member.clone().map(|mem|mem.user.map(|u|u.bot)).flatten().unwrap_or(false);
         trace!("is bot {}", ret);
         ret
     }
