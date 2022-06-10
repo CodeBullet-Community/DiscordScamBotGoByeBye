ca7ed28d74846f500e76bdab0956904f4e6fc0fb
Pagwin <git@pagwin.xyz>
2022-06-10 12:52:55 -0400

    intern: got rid of some dumb comments

--- src/filter_trait.rs
+++ src/filter_trait.rs
@@ -7,7 +7,6 @@ use serenity::{client::Context, model::channel::Message};
 #[async_trait::async_trait]
 pub trait FilterTrait where Self:Send+Sync{
     async fn should_act(&self, message:&Message ,context:&Context)->bool;
-    //don't know why 'static is needed so that could be a problem in the future
     fn and<T>(self, other:T)->AndFilter<Self,T> where T:FilterTrait, Self:Sized{
         return AndFilter(self,other)
     }
@@ -36,9 +35,6 @@ impl<T,U> FilterTrait for AndFilter<T,U> where T:FilterTrait, U:FilterTrait {
         return self.0.should_act(message, context).await && self.1.should_act(message, context).await
     }
 }
-
-//unneeded and enables less readable code but I can't help myself
-//impl
 
 pub struct NegateFilter<T>(T) where T:FilterTrait;
 #[async_trait::async_trait]
